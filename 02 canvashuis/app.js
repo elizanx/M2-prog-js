ac0563bd89bfccfe38fdb78250bdaae48cd9fe11
Reko <36913@ma-web.nl>
2023-11-23 14:41:10 +0100

    push1

---
+++ 02 canvashuis/app.js
@@ -0,0 +1,14 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        console.log(canvas);
+        g.fillRect(0,0,10,10);
+    }
+}
+
+let app = new App();
+app.runApplication();
