e0098e02b27ad527f2ddbe018b4c5d845983f342
Reko <36913@ma-web.nl>
2023-11-23 14:42:35 +0100

    push

--- 01A/app.js
+++ 01A/app.js
@@ -3,15 +3,22 @@ class App
     runApplication()
     {
         console.log("hello world");
-        let appnaam = "App";
-        let versienummer = "0.9"
-        let versiedatum = new Date();
-        let autheur = "Eliza Nabibaks"
-        let copyright = "copyright";
-        let darkmode = true;
+        this.appnaam = "App";
+        this.versienummer = "0.9"
+        this.versiedatum = new Date();
+        this.autheur = "Eliza Nabibaks"
+        this.copyright = "copyright";
+        this.darkmode = true;
         console.log("appnaam: " + appnaam  + "\nversienummer: " + versienummer + "\nversiedatum: " + versiedatum + "\nautheur: " + autheur + "\ncopyright: " + copyright + "\ndarkmode: " + darkmode ) 
+        this.greeting = "Starting up";
     }
 }
 
 let app = new App();
 app.runApplication();
+console.log(app.greeting);
+console.log("appnaam: " + app.appnaam);
+console.log("versienummer: " + app.versienummer);
+console.log("autheur: " + app.autheur);
+console.log("copyright: " + app.copyright);
+console.log("darkmode: " + app.darkmode);
