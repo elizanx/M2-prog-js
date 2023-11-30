96a9b62312b331d01df51fcfd28040f66da405e4
Reko <36913@ma-web.nl>
2023-11-30 19:10:10 +0100

    opdr1

---
+++ 03 ifelse/app.js
@@ -0,0 +1,15 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        //document.getElementById("newstitle")
+        //document.getElementByClassName("gamesnews headline")
+        //document.getElementByClassName("gamenews")
+       
+    }
+}
+
+let app = new App();
+app.runApplication();
+
