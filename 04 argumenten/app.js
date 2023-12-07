a863d33ace1a2056da9161020a085b04228f3f0c
Reko <36913@ma-web.nl>
2023-12-07 14:21:51 +0100

    argumenten

---
+++ 04 argumenten/app.js
@@ -0,0 +1,25 @@
+function argumentsAreHandy(shoutout)
+{
+    console.log("do you want to give a shoutout?");
+    console.log(shoutout);
+}
+argumentsAreHandy("Super Shout Out!");
+argumentsAreHandy("Super!");
+argumentsAreHandy("Shout out!")
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        
+        
+    }
+}
+
+let app = new App();
+app.runApplication();
+
+
+
+
+
